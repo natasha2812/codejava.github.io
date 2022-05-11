85d64257066427b11e85bcdfc5bddf1a28dd2379
natasha2812 <105270787+natasha2812@users.noreply.github.com>
2022-05-11 12:44:25 +0530

    commit 11

---
+++ abcd/src/abcd1/abcd11.java
@@ -0,0 +1,14 @@
+package abcd1;
+
+public class abcd11 {
+
+	public abcd11() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello");
+	}
+
+}
